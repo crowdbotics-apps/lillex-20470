60035cbd4ac3f0ef49221100571c528105fc3ea1
Crowdbotics <team@crowdbotics.com>
2020-09-20 03:24:00 +0000

    Update models

--- backend/lillex_20470/wsgi.py
+++ backend/lillex_20470/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lillex_20470.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lillex_20470.settings")
 
 application = get_wsgi_application()
